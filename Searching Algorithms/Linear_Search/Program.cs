20aa9eac5fd09003320849d94cd0d896704e4f5e
DerPommeDeTerre <leobardo.jmaya@gmail.com>
2025-09-05 20:15:55 -0600

    linear search

---
+++ Searching Algorithms/Linear_Search/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Search {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+            
+            int target = 30;
+            int result = Search.LinearSearch(numbers, target);
+
+            if(result != -1) {
+                Console.WriteLine("Element found at index: " + result);
+            } else {
+                Console.WriteLine("Element not found");
+            }
+        }
+    }
+}
---
+++ Searching Algorithms/Linear_Search/Search.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Search {
+    internal class Search {
+        public static int LinearSearch(int[] arr, int target) {
+            for(int i = 0; i < arr.Length; i++) {
+                if (arr[i] == target) {
+                    return i;//Returns the position in the array
+                }
+            }
+            return -1;//Value used in the condition
+        }
+    }
+}
