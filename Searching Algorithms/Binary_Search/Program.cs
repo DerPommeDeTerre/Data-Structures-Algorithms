069f24c09820a28168b93d95a284954d4ea88f16
DerPommeDeTerre <leobardo.jmaya@gmail.com>
2025-09-08 14:12:44 -0600

    Binary_Search

---
+++ Searching Algorithms/Binary_Search/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search {
+    internal class Program {
+        static void Main(string[] args) {
+
+            //int[] numbers = { 10, 20, 30, 40, 50 };
+            //int target = 30;
+
+            int[] numbers = GenerateSortUserIds(100);
+            Random rand = new Random();
+            int target = numbers[rand.Next(numbers.Length)];
+            Console.WriteLine("Target is: " + target);
+
+
+            int result = Search.BinarySearch(numbers, target);
+
+            if (result != -1) {
+                Console.WriteLine("Element foud at index: " + result);
+            } else {
+                Console.WriteLine("Element not found.");
+            }
+        }
+        static int[] GenerateSortUserIds(int n) { //Method to populate the ARRAY
+            int[] ids = new int[n];
+            for (int i = 0; i < n; i++) {
+                ids[i] = i + 1;
+            }
+            return ids;
+        }
+    }
+}
---
+++ Searching Algorithms/Binary_Search/Search.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search {
+    internal class Search {
+
+        public static int BinarySearch(int[] arr, int target) {
+            
+            int left = 0; //Beginning of the array
+            int right = arr.Length - 1; //End of the array
+            
+            while(left <= right) {
+                int mid = left + (right - left) / 2; //MIDDLE index calculation
+                if (arr[mid] == target) {
+                    Console.WriteLine("Middle is: " + arr[mid]);
+                    return mid;
+                } else if (arr[mid] < target) {
+                    Console.WriteLine("Middle is: " + arr[mid]);
+                    left = mid + 1;
+                } else {
+                    Console.WriteLine("Middle is: " + arr[mid]);
+                    right = mid - 1;
+                }
+            }
+            return -1; //VALUE was not found
+        }
+    }
+}
