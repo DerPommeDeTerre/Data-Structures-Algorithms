7757582db999417338d2000ef17a3a06c0bd006c
DerPommeDeTerre <leobardo.jmaya@gmail.com>
2025-09-11 23:26:21 -0600

    insertion sort

---
+++ Sorting and Searching Algorithms/InsertionSort_EXTRA/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace InsertionSort_EXTRA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 9, 1, 8, 2, 7, 3, 6, 5, 4 };
+
+            insertionSort(numbers);
+
+            foreach(int element in numbers) {
+                Console.Write(element + " ");
+            }
+        }
+        static void insertionSort(int[] numbers) {
+            for (int i = 1; i < numbers.Length; i++) {
+                int temp = numbers[i];
+                int j = i - 1;
+                while (j >= 0 && numbers[j] > temp) {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+}
